c3083a72fc00002db3d0d17ebc526f395a69a5ba
Heeralalgurjar <145098823+Heeralalgurjar@users.noreply.github.com>
2024-09-26 13:50:32 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,17 @@
+const name ="Heeralal"
+const repoCount = 50
+
+// console.log(name + repoCount + ' value')
+
+// console.log(`my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Heerall')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+console.log(gameName.indexOf('a'));
